b640830904abfe047d2b546186a281ee2e092194
Ashutosh119 <ashutoshmca119@gmail.com>
2025-11-17 14:51:11 +0530

    initail changes

--- src/main/java/com/luv2code/books/BooksApplication.java
+++ src/main/java/com/luv2code/books/BooksApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BooksApplication.class, args);
+
+        SpringApplication.run(BooksApplication.class, args);
+        System.out.println("RESTful Service for Managing Book Data");
 	}
 
 }
